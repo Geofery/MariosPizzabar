b85a2d2dbed967103d2f667df948a58fc9a10219
Roed <Geofery10@hotmail.com>
2021-04-19 11:43:00 +0200

    Oprettelse af classes vi skal bruge

--- Pizza.java
+++ Pizza.java
@@ -4,10 +4,12 @@ public class Pizza {
   private String name;
   private int number;
   private ArrayList<String> toppings;
+  private int price;
 
-  public Pizza(String name, int number, ArrayList toppings){
+  public Pizza(String name, int number, ArrayList toppings, int price){
     this.name = name;
     this.number = number;
     this.toppings = toppings;
+    this.price = price;
   }
 }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MariosPizzabar
 Projekt 1 Semester Datamatiker
+Gruppe 2:
+Michala: https://github.com/MichalaNybroe
+Jens: https://github.com/Jens-k-m-m
+August: https://github.com/TheeCapain
+Jeff: https://github.com/Geofery
