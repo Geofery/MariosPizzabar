d30ebc6dbd88995db9a201b1a9df10162c03bc39
Roed <Geofery10@hotmail.com>
2021-04-20 11:28:43 +0200

    ArrayListe med Pizzaer

--- MariosPizzabar/src/MariosPizzabarMenu.java
+++ MariosPizzabar/src/MariosPizzabarMenu.java
@@ -1,37 +1,43 @@
+import java.util.ArrayList;
+
 public class MariosPizzabarMenu {
 
   public void createPizzas(){
     // reference menu
     //https://www.colaccio.com/english-menu/
-    Pizza margheriata = new Pizza("Margheriata", 1,"Tomato sauce, Cheese" , 100);
-    Pizza hawaii = new Pizza("Hawaii", 2, "Ham, Pineapple", 100);
-    Pizza venezia = new Pizza("Venezia", 3, "Salami, Pepper, Onion, Olive", 100);
-    Pizza fungi = new Pizza("Fungi", 4, "Mushroom", 100);
-    Pizza pompei = new Pizza("Pompei", 5, "Bacon, onions", 100);
-    Pizza marinara = new Pizza("Marinara", 6, "Shrimp, Clam", 100);
-    Pizza pescatore = new Pizza("Pescatore", 7, "Salami, Bacon, Egg", 100);
-    Pizza bussola = new Pizza("Bussola", 8, "Ham, Shrimp", 100);
-    Pizza vegetarian = new Pizza("Vegetarian", 9, "Mushroom, Pepper, Onion, Artichole, Olive, Tomato", 100);
-    Pizza natural = new Pizza("Natural", 10, "Mushroom, Pepper, Onion, Artichoke, Olive, Pinapple", 100);
-    Pizza vesuvio = new Pizza("Vesuvio", 11, "Ham", 100);
-    Pizza havanna = new Pizza("Havanna", 12, "Shrimp, tuna", 100);
-    Pizza capricciosa = new Pizza("Capricciosa", 13, "Ham, Mushroom", 100);
-    Pizza bologenese = new Pizza("Bologenese", 14, "Ground beef, Onion", 100);
-    Pizza calzone = new Pizza("Calzone", 15, "(baked) Ham", 120);
-    Pizza minnenAthen = new Pizza("Minnen Athen", 16, "Ground beef, Tomato, Feta cheese", 140);
-    Pizza amore = new Pizza("Amore", 17, "Tuna, Mushroom", 120);
-    Pizza napolitana = new Pizza("Napolitana", 18, "Sardine, Olive", 155);
-    Pizza morzzarella = new Pizza("Morzzarella", 19, "Mozzarella, Ruccola, Svartpeppar", 110);
-    Pizza colaccio = new Pizza("Colaccio", 20, "Ham, Shrimp, Mushroom", 120);
-    Pizza peperoni = new Pizza("Peperoni", 21, "Pepperoni sausage, Salami, Olive, Pepper", 150);
-    Pizza tropicana = new Pizza("Tropicana", 22, "Ham, Banana, Pineapple, Curry", 145);
-    Pizza oxfiletPizza = new Pizza("Oxfilet Pizza", 23, "Fillet of beef, Mushroom, Tomato, Onions, Bearnaise sauce", 180);
-    Pizza quattroStagioni = new Pizza("Quattro Stagioni", 24, "Ham, Shrimp, Clam, Mushroom, Artichoke, Olive", 120);
-    Pizza gorgonzola = new Pizza("Gorgonzola", 25, "Ham, Tomato, Gorgonzola", 130);
-    Pizza provencale = new Pizza("Provencale", 26, "Pork, Mushroom, Garlic, Onion", 115);
-    Pizza mafioso = new Pizza("Mafioso", 27, "Salami, Tomato, Pepperoni, Onion, Cayenne pepper", 110);
-    Pizza pizzaCarne = new Pizza("Pizza Carne", 28, "Ground beef, Pepper, Onion, Egg", 125);
-    Pizza melina = new Pizza("Melina", 29, "Mushroom, Tomato, Leek, Olive, Feta cheese", 130);
-    Pizza Jens = new Pizza("Jens Special", 30, "Ham, Pineapple, Tomato, Cheese, Olive, Chili, Gorgonzola, Banana", 210);
+    ArrayList<Pizza> mariosPizzaMenu = new ArrayList<>();
+    mariosPizzaMenu.add(new Pizza("Margheriata", 1,"Tomato sauce, Cheese" , 100));
+    mariosPizzaMenu.add(new Pizza("Hawaii", 2, "Ham, Pineapple", 100));
+    mariosPizzaMenu.add(new Pizza("Venezia", 3, "Salami, Pepper, Onion, Olive", 100));
+    mariosPizzaMenu.add(new Pizza("Fungi", 4, "Mushroom", 100));
+    mariosPizzaMenu.add(new Pizza("Pompei", 5, "Bacon, onions", 100));
+    mariosPizzaMenu.add(new Pizza("Marinara", 6, "Shrimp, Clam", 100));
+    mariosPizzaMenu.add(new Pizza("Pescatore", 7, "Salami, Bacon, Egg", 100));
+    mariosPizzaMenu.add(new Pizza("Bussola", 8, "Ham, Shrimp", 100));
+    mariosPizzaMenu.add(new Pizza("Vegetarian", 9, "Mushroom, Pepper, Onion, Artichole, Olive, Tomato", 100));
+    mariosPizzaMenu.add(new Pizza("Natural", 10, "Mushroom, Pepper, Onion, Artichoke, Olive, Pinapple", 100));
+    mariosPizzaMenu.add(new Pizza("Vesuvio", 11, "Ham", 100));
+    mariosPizzaMenu.add(new Pizza("Havanna", 12, "Shrimp, tuna", 100));
+    mariosPizzaMenu.add(new Pizza("Capricciosa", 13, "Ham, Mushroom", 100));
+    mariosPizzaMenu.add(new Pizza("Bologenese", 14, "Ground beef, Onion", 100));
+    mariosPizzaMenu.add(new Pizza("Calzone", 15, "(baked) Ham", 120));
+    mariosPizzaMenu.add(new Pizza("Minnen Athen", 16, "Ground beef, Tomato, Feta cheese", 140));
+    mariosPizzaMenu.add(new Pizza("Amore", 17, "Tuna, Mushroom", 120));
+    mariosPizzaMenu.add(new Pizza("Napolitana", 18, "Sardine, Olive", 155));
+    mariosPizzaMenu.add(new Pizza("Morzzarella", 19, "Mozzarella, Ruccola, Svartpeppar", 110));
+    mariosPizzaMenu.add(new Pizza("Colaccio", 20, "Ham, Shrimp, Mushroom", 120));
+    mariosPizzaMenu.add(new Pizza("Peperoni", 21, "Pepperoni sausage, Salami, Olive, Pepper", 150));
+    mariosPizzaMenu.add(new Pizza("Tropicana", 22, "Ham, Banana, Pineapple, Curry", 145));
+    mariosPizzaMenu.add(new Pizza("Oxfilet Pizza", 23, "Fillet of beef, Mushroom, Tomato, Onions, Bearnaise sauce", 180));
+    mariosPizzaMenu.add(new Pizza("Quattro Stagioni", 24, "Ham, Shrimp, Clam, Mushroom, Artichoke, Olive", 120));
+    mariosPizzaMenu.add(new Pizza("Gorgonzola", 25, "Ham, Tomato, Gorgonzola", 130));
+    mariosPizzaMenu.add(new Pizza("Provencale", 26, "Pork, Mushroom, Garlic, Onion", 115));
+    mariosPizzaMenu.add(new Pizza("Mafioso", 27, "Salami, Tomato, Pepperoni, Onion, Cayenne pepper", 110));
+    mariosPizzaMenu.add(new Pizza("Pizza Carne", 28, "Ground beef, Pepper, Onion, Egg", 125));
+    mariosPizzaMenu.add(new Pizza("Melina", 29, "Mushroom, Tomato, Leek, Olive, Feta cheese", 130));
+    mariosPizzaMenu.add(new Pizza("Jens Special", 30, "Ham, Pineapple, Tomato, Cheese, Olive, Chili, Gorgonzola, Banana", 210));
   }
+
+
+
 }
