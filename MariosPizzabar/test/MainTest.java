df265299e6ed6fba051bc1528f8c59dc75620b9e
Michala Nybroe <michalanim@gmail.com>
2021-04-21 13:22:30 +0200

    test of main

---
+++ MariosPizzabar/test/MainTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+  @Test
+  void showMenu() {
+  }
+
+  @Test
+  void showOrderList() {
+  }
+
+  @Test
+  void addOrder() {
+  }
+}
