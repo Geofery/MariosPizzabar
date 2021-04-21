90800bedb7c09a7a5ac39c9166588ca078bf6ce5
Michala Nybroe <michalanim@gmail.com>
2021-04-21 16:12:05 +0200

    merge

--- MariosPizzabar/src/Menu.java
+++ MariosPizzabar/src/Menu.java
@@ -1,49 +1,49 @@
 import java.util.Scanner;
 
 public class Menu {
-    private String menuHeader;
-    private String leadText;
-    private String[] menuItems;
+  private String menuHeader;
+  private String leadText;
+  private String[] menuItems;
 
-    UI ui = new UI();
+  UI ui = new UI();
 
-    public Menu(String menuHeader, String leadText) {
-        this.menuHeader = menuHeader;
-        this.leadText = leadText;
+  public Menu(String menuHeader, String leadText) {
+    this.menuHeader = menuHeader;
+    this.leadText = leadText;
 
-        String[] menuItems = {
-            "1. Show menu",
-            "2. Show orderlist",
-            "3. Add order",
-            "4. Remove order",
-            "5. Order finished",
-            "6. Show statistics",
-            "9. Quit"
-        };
+    String[] menuItems = {
+        "1. Show menu",
+        "2. Show orderlist",
+        "3. Add order",
+        "4. Remove order",
+        "5. Order finished",
+        "6. Show statistics",
+        "9. Quit"
+    };
 
-        this.menuItems = menuItems;
+    this.menuItems = menuItems;
+  }
+
+  public String getLeadText() {
+    return leadText;
+  }
+
+  public void printMenu() {
+    String printMenu = menuHeader + "\n";
+    for (int i = 0; i < menuItems.length; i++) {
+      printMenu += menuItems[i] + "\n";
     }
+    printMenu += leadText;
 
-   public String getLeadText() {
-        return leadText;
-    }
+    ui.getString(printMenu);
+  }
 
-    public void printMenu() {
-        String printMenu = menuHeader + "\n";
-        for (int i = 0; i < menuItems.length; i++) {
-            printMenu += menuItems[i] + "\n";
-        }
-        printMenu += leadText;
-
-        ui.getString(printMenu);
-    }
-
-    public int readChoice() {
-      /*Scanner scanner = new Scanner(System.in);
-        while (!scanner.hasNext()) {
-            ui.getString(getLeadText());
-            scanner.next();
-        }*/
-        return ui.getScanInt();
-    }
+  public int readChoice() {
+    /*Scanner scanner = new Scanner(System.in);
+    while (!scanner.hasNextInt()) {
+      ui.getString(getLeadText());
+      scanner.next();
+    }*/
+    return ui.getScanInt();
+  }
 }
