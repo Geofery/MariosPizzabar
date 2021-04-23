1f18ce4ff9b96994f42cefcee131dd1301e4576a
Jens-k-m-m <jensmonteiro@gmail.com>
2021-04-23 11:26:07 +0200

    test ordre

--- MariosPizzabar/Test/OrderListTest.java
+++ MariosPizzabar/Test/OrderListTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,35 +10,83 @@ class OrderListTest {
 
   @Test
   void addOrder() {
+    //Arrange
+    int orderNr = 1;
+    String time = "21-04-23 09:58";
 
+    ArrayList<Pizza> pizzas = new ArrayList<>();
+    Pizza pizza = new Pizza("Jens", 1, "ost", 100);
+    pizzas.add(pizza);
+
+    ArrayList<Order> orders = new ArrayList<>();
+    orders.add(new Order(orderNr, time, pizzas));
+
+    int expected = 1;
+
+    //Act
+    int actual = orders.size();
+
+    // Assert
+    assertEquals(expected, actual);
   }
 
   @Test
   void generateOrderNr() {
-//Arrange
+    //Arrange
     ArrayList<Integer> OrderList = new ArrayList<Integer>();
     OrderList ordernr = new OrderList();
     int actual = OrderList.size() + 1;
-//Act
+
+    //Act
     int expected = 1;
 
-// Assert
+    // Assert
     assertEquals(expected, actual);
 
   }
 
   @Test
-  void houseOrPhoneOrder() {
+  void houseOrPhoneOrderP() {
+    //husk at ændre tid og dato når du tester til din ejen pc tid
+    String expectedP = "21-04-23 11:38";
+
+    LocalDateTime actualp = LocalDateTime.now().plusHours(1);
+    DateTimeFormatter time = DateTimeFormatter.ofPattern("yy-MM-dd HH:mm");
+
+    //Act
+    String actual = actualp.format(time);
+
+    //Assert
+    assertEquals(expectedP, actual);
+  }
+
+  @Test
+  void houseOrPhoneOrderH() {
+    //Arrange
+    //husk at ændre tid og dato når du tester til din ejen pc tid
+    String expectedH = "21-04-23 11:06";
+
+    LocalDateTime actualh = LocalDateTime.now().plusMinutes(25);
+    DateTimeFormatter time = DateTimeFormatter.ofPattern("yy-MM-dd HH:mm");
+
+    //Act
+    String actual = actualh.format(time);
+
+    //Assert
+    assertEquals(expectedH, actual);
   }
 
   @Test
   void generateDateTime() {
 
-    //Arrange
-    LocalDateTime expected = LocalDateTime.now().plusHours(1);
+    //Arrange husk at ændre tid og dato når du tester til din ejen pc tid
+    String expected = "21-04-23 09:58";
 
     //Act
-    LocalDateTime actual = LocalDateTime.now().plusHours(1);
+    LocalDateTime actual1 = LocalDateTime.now();
+    DateTimeFormatter time = DateTimeFormatter.ofPattern("yy-MM-dd HH:mm");
+    String actual = actual1.format(time);
+
     //Assert
 
     assertEquals(expected, actual);
--- MariosPizzabar/Test/UIStub.java
+++ MariosPizzabar/Test/UIStub.java
@@ -1,11 +1,22 @@
-public class UIStub extends UI{
+public class UIStub extends UI {
   private int nextValue;
+  private String nextValueS;
 
- public int getScanInt() {
-   return nextValue;
- }
 
- public void confirmGetScanInt(int nextValue) {
-   this.nextValue = nextValue;
- }
+  public int getScanInt() {
+    return nextValue;
+  }
+
+  public String getScanString() {
+    return nextValueS;
+  }
+
+  public void confirmGetScanInt(int nextValue) {
+    this.nextValue = nextValue;
+  }
+
+  public void confirmGetScanString(String nextValue) {
+    this.nextValueS = nextValue;
+
+  }
 }
