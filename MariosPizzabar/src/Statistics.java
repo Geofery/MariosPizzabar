bb7d8e7c4aba0188dd62674da3a5bd50e9f5359a
August <AugustHauerslev@outlook.com>
2021-04-23 09:41:39 +0200

    Started Statistics

--- MariosPizzabar/src/Statistics.java
+++ MariosPizzabar/src/Statistics.java
@@ -1,2 +1,18 @@
 public class Statistics {
+    private int total;
+
+    public int countTotal(OrderList orderList) {
+        for (int i = 0; i < orderList.getOrders().size(); i++) {
+            total = orderList.getTotal();
+        }
+        return total;
+    }
+
+    public int getTotalCash() {
+        return total;
+    }
+
+    public void nrOfPizzaSold() {
+
+    }
 }
