6c40c023de0346554542510f782c4a5f59107371
Michala Nybroe <michalanim@gmail.com>
2021-04-24 13:35:24 +0200

    test gjort klar 13:35

--- MariosPizzabar/test/MainTest.java
+++ MariosPizzabar/test/MainTest.java
@@ -1,16 +1,29 @@
 import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
-
 class MainTest {
 
   @Test
-  void addOrder() throws FileNotFoundException {
+  void addOrder(){
     UIStub uiStub = new UIStub();
     Main main = new Main(uiStub);
     uiStub.confirmGetScanInt(3);
     main.run();
     //derefter tjekkes metoder i orderList
     //altså addOrder og de indlejrede metoder deri. kan laves når hele "flowet" er lavet
+  }
+
+  @Test
+  void markAsFinished() {
+
+  }
+
+  @Test
+  void cancelOrder() {
+
+  }
+
+  @Test
+  void getTotalOfDay() {
+    
   }
 }
